462b9f06b90eaea3859172281f9b933716a8bfbd
yashwanth <yashwanthreddy7675@gmail.com>
2026-02-08 22:19:18 +0530

    ui releated to offers

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { socket } from '../utils/socket';
+import { useMediaState } from '../utils/store/useMediaState';
 
 
 export const Landing = () => {
   const navigate = useNavigate();
   const [roomId, setRoomId] = React.useState('');
+  const [name, setName] = useState("");
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const getCam = async () => {
+        const stream = await window.navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        })
+        // MediaStream
+        const audioTrack = stream.getAudioTracks()[0]
+        const videoTrack = stream.getVideoTracks()[0]
+    
+        useMediaState.getState().setLocalTracks(videoTrack, audioTrack);
+        if (!videoRef.current) {
+            return;
+        }
+        videoRef.current.srcObject = new MediaStream([videoTrack])
+        videoRef.current.play();
+        // MediaStream
+    }
+
+    useEffect(() => {
+        if (videoRef && videoRef.current) {
+            getCam()
+        }
+    }, [videoRef]);
 
   function handleRoomNavigation(roomId: string) {
     navigate(`/room/${roomId}`);
@@ -27,9 +54,13 @@ export const Landing = () => {
       handleRoomNavigation(roomId);
     });
   }
-
   return (
     <div className="flex flex-col gap-6">
+       <video className='w-40 h-40'  autoPlay ref={videoRef} muted></video>
+            <input type="text" onChange={(e) => {
+                setName(e.target.value);
+            }}>
+            </input>
       <button className="w-40 bg-green-300" onClick={handleCreateRoom}>
         Create Room
       </button>
